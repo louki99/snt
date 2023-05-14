1f7e27eeb2cfc75aec81952b11aed75cffb41a76
Louki99 <louki.idris@gmail.com>
2023-05-14 16:57:13 +0100

    add theme

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import './bootstrap';
+
+
+import '../theme/css/style.css';
+import '../theme/css/widgets.css';
+import '../theme/css/responsive.css';
+import '../theme/css/custom.css';
+
+
+import '../theme/js/vendor/jquery-3.5.1.min';
+import '../theme/js/vendor/slick.min';
+import '../theme/js/vendor/wow.min';
+import '../theme/js/vendor/jquery.scrollUp.min';
+import '../theme/js/vendor/jquery.sticky';
+import '../theme/js/vendor/perfect-scrollbar';
+import '../theme/js/vendor/jquery.waypoints.min';
+import '../theme/js/vendor/jquery.theia.sticky';
+import '../theme/js/vendor/jquery.slicknav';
+import '../theme/js/main';
