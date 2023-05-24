ad2816e4981c6923207d32e866ee02999d8b9735
Louki99 <louki.idris@gmail.com>
2023-05-24 18:19:55 +0100

    .

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,34 +1,20 @@
 import './bootstrap';
 
-
-import '../theme/css/vendor/bootstrap.min.css';
-import '../theme/css/vendor/owl.carousel.min.css';
-import '../theme/css/vendor/ticker-style.css';
-import '../theme/css/vendor/elegant-icons.css';
-import '../theme/css/vendor/slick.css';
-import '../theme/css/vendor/slicknav.css';
-import '../theme/css/vendor/animate.min.css';
-import '../theme/css/vendor/nice-select.css';
-import '../theme/css/vendor/perfect-scrollbar.css';
-
 import '../theme/css/style.css';
 import '../theme/css/subMenu.css';
 import '../theme/css/widgets.css';
 import '../theme/css/responsive.css';
 import '../theme/css/custom.css';
 import '../theme/css/vendor/slick.css';
-// import '../theme/css/vendor/bootstrap.min.css';
 import '../css/app.css';
 
 
 import '../theme/js/vendor/modernizr-3.5.0.min';
-// import '../theme/js/vendor/jquery-3.5.1.min';
 import '../theme/js/vendor/slick.min';
 import '../theme/js/vendor/jquery.ticker';
 import '../theme/js/vendor/jquery.vticker-min';
 import '../theme/js/vendor/jquery.nice-select.min';
 import '../theme/js/vendor/jquery.magnific-popup';
-// import '../theme/js/vendor/wow.min';
 import '../theme/js/vendor/jquery.scrollUp.min';
 import '../theme/js/vendor/jquery.sticky';
 import '../theme/js/vendor/perfect-scrollbar';
@@ -36,6 +22,5 @@ import '../theme/js/vendor/jquery.waypoints.min';
 import '../theme/js/vendor/jquery.theia.sticky';
 import '../theme/js/vendor/jquery.slicknav';
 import '../theme/js/vendor/bootstrap.min';
-
 import '../theme/js/main';
-import '../theme/js/custom'
+import '../theme/js/custom';
--- resources/views/snt/main.blade.php
+++ resources/views/snt/main.blade.php
@@ -9,20 +9,13 @@
     <script src="{{ asset("js/wow.min.js") }}"></script>
 
     <script src="{{ asset("js/jquery.min.js") }}"></script>
-    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> --}}
-    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.0.8/popper.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script> --}}
-    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script> --}}
-
     <script src="{{ asset("js/jquery.fancybox.min.js") }}"></script>
 
     <link rel="stylesheet" href="{{ asset("css/jquery.fancybox.css") }}">
 
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.css"> --}}
-
     <link href="https://fonts.cdnfonts.com/css/open-sans" rel="stylesheet">
 
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
         :root {
